c692b3398cc932b02787d0964de141725017f326
Nikolay <kolune4ka@mail.ru>
2023-02-13 18:41:29 +0300

    Add homework4

---
+++ DZ_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+inDegree(numberA, numberB);
+
+
+void inDegree(int a, int b)
+{
+    int result = 1;
+
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string argument)
+{
+    Console.WriteLine(argument);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ DZ_3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу, которая принимает на вход число и выдает количество цифр в числе
+Console.WriteLine(CalculateSumOfDigits(ReadInt("number")));
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument} ");
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It's not an int");
+    }
+
+    return number;
+}
+
+int CalculateSumOfDigits(int number)
+{
+    int sum = 0;
+    string s = number.ToString();
+
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        sum ++;
+    }
+
+    return sum;
+}
